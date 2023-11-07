85183fe3514f3bd18545781b8842458941c9ef56
suleman893 <sulemanahmad893@gmail.com>
2023-11-07 16:26:37 +0500

    Basic setup done

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,38 @@
+import { Alert, Button, Col, Form, Row, Stack } from "react-bootstrap";
+
+const Register = () => {
+  return (
+    <>
+      <Form>
+        <Row
+          style={{
+            height: "100vh",
+            justifyContent: "center",
+            paddingTop: "10%",
+          }}
+        >
+          <Col xs={6}>
+            <Stack gap={3}>
+              <h2>Register</h2>
+              <Form.Control type="text" placeholder="Name" />
+
+              <Form.Control type="email" placeholder="Email" />
+
+              <Form.Control type="password" placeholder="Password" />
+
+              <Button variant="primary " type="submit">
+                Register
+              </Button>
+
+              <Alert variant="danger">
+                <p>An error occurred</p>
+              </Alert>
+            </Stack>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
+};
+
+export default Register;
---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cors());
+app.use("/api/users", userRoutes);
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
+
+app.listen(port, (req, res) => {
+  console.log("Server running on port", port);
+});
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((error) => console.log("DB Connection error", error.message));
